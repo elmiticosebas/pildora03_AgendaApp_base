9ed6ef695a27441245ecd2d29a7fcdf53f8d6998
hgarp <hgarper2006@gmail.com>
2025-10-01 12:47:31 +0100

    asdf

--- src/org/ulpgc/is1/model/Contact.java
+++ src/org/ulpgc/is1/model/Contact.java
@@ -1,5 +1,23 @@
 package org.ulpgc.is1.model;
 
 public abstract class Contact {
-
+    String telephone;
+    String email;
+    Address address;
+    public Contact(String telephone, String email, Address address) {
+        this.telephone = telephone;
+        this.email = email;
+        this.address = address;
+    }
+    public String getTelephone() {
+        return this.telephone;
+    }
+    public void setTelephone(String telephone) {};
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) {};
+    public Address getAddress() {
+        return this.address;
+    }
 }
--- src/org/ulpgc/is1/model/Person.java
+++ src/org/ulpgc/is1/model/Person.java
@@ -1,5 +1,15 @@
 package org.ulpgc.is1.model;
 
 public class Person extends Contact {
+    String firstName;
+    String lastName;
+    public Person(String firstName, String lastName, String address, String email, String telephone) {
+        super(telephone, email, address);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
+    public String getName() {
+        return firstName + lastName;
+    }
 }
